57de824ff5682ee8146c8c6b112a2b4a031b89dd
DonggiYang <luxdefuror@gmail.com>
2023-03-10 16:24:07 +0900

    chore: Springboot initial commit

---
+++ sansam/src/test/java/com/sansam/SansamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sansam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SansamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
