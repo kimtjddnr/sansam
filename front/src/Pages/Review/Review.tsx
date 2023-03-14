cdfdf8df4f97a47676aeaaaa213e33bf43ad4dae
yunseonyoung <sunysa8@naver.com>
2023-03-14 16:31:20 +0900

    feat: Register react-router

--- front/src/Pages/Review/Review.tsx
+++ front/src/Pages/Review/Review.tsx
@@ -1,1 +1,9 @@
-export {};
+function Review() {
+  return (
+    <div className="Review">
+      <h1>Review</h1>
+    </div>
+  );
+}
+
+export default Review;
--- front/src/Pages/SignUp/SignUp.tsx
+++ front/src/Pages/SignUp/SignUp.tsx
@@ -1,1 +1,9 @@
-export {};
+function SignUp() {
+  return (
+    <div className="SignUp">
+      <h1>SignUp</h1>
+    </div>
+  );
+}
+
+export default SignUp;
