ddd8310796d7ef2cefe38d1615f92408d00596b9
sunysa8 <sunysa8@naver.com>
2023-03-16 00:06:30 +0900

    feat: implemented Kakao Login Function

---
+++ front/src/Pages/Start/KakaoLoginData.js
@@ -0,0 +1,2 @@
+export const REST_API_KEY = "3c83f92795e8378034ffbd8b1f07d08d";
+export const REDIRECT_URI = "http://localhost:3000/login";
--- front/src/Pages/Start/Start.tsx
+++ front/src/Pages/Start/Start.tsx
@@ -1,9 +1,39 @@
-function Start() {
+import { REST_API_KEY, REDIRECT_URI } from "./KakaoLoginData";
+
+const Start = () => {
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  const handleLogin = () => {
+    window.location.href = KAKAO_AUTH_URL;
+  };
+
   return (
     <div className="Start">
-      <h1>Start</h1>
+      <h1>Start입니다</h1>
+      <img
+        src="img/kakao_login_medium_narrow.png"
+        alt="login"
+        onClick={handleLogin}
+      />
+      <img
+        src="img/kakao_login_medium_wide.png"
+        alt="login_2"
+        onClick={handleLogin}
+      />
     </div>
   );
-}
+};
+
+// function Start() {
+//   return (
+//     <div className="Start">
+//       <h1>Start</h1>
+//       {/* <img src="img/kakao_login_large_narrow.png" alt="logo1" />
+//       <img src="img/kakao_login_large_wide.png" alt="logo1" /> */}
+//       <img src="img/kakao_login_medium_narrow.png" alt="logo2" />
+//       <img src="img/kakao_login_medium_wide.png" alt="logo2" />
+//     </div>
+//   );
+// }
 
 export default Start;
