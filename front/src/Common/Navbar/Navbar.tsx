92521dabff59c63d4b7abd069b33e472f7c8cfc9
ahyunnn <ahyun3089@gmail.com>
2023-03-17 00:09:58 +0900

    feat: [Navbar] / create navbar, hamburgerBtn

--- front/src/Common/Navbar/Navbar.tsx
+++ front/src/Common/Navbar/Navbar.tsx
@@ -1,12 +1,31 @@
-import React from "react";
-import "./Navbar.css";
+import styled from "styled-components";
+import HamBtn from "./HamBtn";
 
 function Navbar() {
   return (
-    <div className="Navbar">
-      <img className="logoImg" src="/img/logo2.png" alt="logo" />
-    </div>
+    <StyledDiv className="Navbar">
+      <StyledImg className="logoImg" src="/img/logo2.png" alt="logo" />
+      <HamBtn />
+    </StyledDiv>
   );
 }
+
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  position: sticky;
+  height: 50px;
+  width: 100%;
+  background-color: white;
+  margin: 0;
+  padding: 0;
+  border: 2px solid #8bcfa2;
+`;
+
+const StyledImg = styled.img`
+  margin-left: 8px;
+  margin-top: 1px;
+  height: 60px;
+`;
 
 export default Navbar;
