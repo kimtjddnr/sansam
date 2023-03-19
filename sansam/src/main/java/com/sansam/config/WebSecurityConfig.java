a5c0f4aa882393f102e499bd8f40ed139e037100
DonggiYang <xronace@gmail.com>
2023-03-19 22:04:07 +0900

    feat: added security filterchain to fetch OAuth login.

--- sansam/src/main/java/com/sansam/config/WebSecurityConfig.java
+++ sansam/src/main/java/com/sansam/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.sansam.config;
 
 import com.sansam.config.jwt.JwtAuthenticationFilter;
 import com.sansam.config.jwt.JwtProvider;
+import com.sansam.service.OAuth2UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -16,23 +19,38 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class WebSecurityConfig {
 
     private final JwtProvider jwtProvider;
+
+    private final OAuth2UserServiceImpl oAuth2UserServiceImpl;
+
+    private final AuthenticationSuccessHandler authenticationSuccessHandler;
+
+    private final AuthenticationFailureHandler authenticationFailureHandler;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        return http
-                .formLogin().disable()
-                .httpBasic().disable()
-                .csrf().disable()
-                .headers().frameOptions().disable()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS).permitAll()
+        http
+            .formLogin().disable()
+            .httpBasic().disable()
+            .csrf().disable()
+            .headers().frameOptions().disable()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests()
+            .antMatchers(HttpMethod.OPTIONS).permitAll()
+            .antMatchers("/", "/login/*").permitAll()
 //                .antMatchers("/", "/auth/**", "/js/**", "/image/**", "/webjars/**").permitAll()
-                .and()
-                .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
-                .build();
+            .and()
+            .oauth2Login()
+            .userInfoEndpoint().userService(oAuth2UserServiceImpl)
+            .and()
+            .successHandler(authenticationSuccessHandler)
+            .failureHandler(authenticationFailureHandler);
+        http
+            .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
