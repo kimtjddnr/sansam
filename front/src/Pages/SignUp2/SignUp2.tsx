a7cd71474c171494e9ae39b5ecc251309c9a6f00
이진우 <blu_mountain@naver.com>
2023-03-20 16:18:21 +0900

    refactor: [SignUp1.tsx] / name input div tag removed & unnecessary code removed

--- front/src/Pages/SignUp2/SignUp2.tsx
+++ front/src/Pages/SignUp2/SignUp2.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 import "./SignUp2.css";
-import SearchBar from './Components/SearchBar';
-import ExperienceList from './Components/ExperienceList';
-
+import SearchBar from "./Components/SearchBar";
+import ExperienceList from "./Components/ExperienceList";
 
 function SignUp2() {
   return (
     <div className="SignUp2">
       <h1>Navbar is here</h1>
-      <div className='SignUp2Title'>
-        <p>최근 1년 내 다녀온</p><p>등산 경험을 입력해주세요.</p>
+      <div className="SignUp2Title">
+        <p>최근 1년 내 다녀온</p>
+        <p>등산 경험을 입력해주세요.</p>
       </div>
       <SearchBar />
       <ExperienceList />
