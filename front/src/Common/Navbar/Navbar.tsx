1a571bd3d16c2b97ac93b98fb8c14e099668cced
이아현 <ahyun3089@naver.com>
2023-03-20 14:18:02 +0900

    merge: feat/369 to feat/139

---
+++ front/src/Common/Navbar/Navbar.tsx
@@ -0,0 +1,50 @@
+import { useState} from "react";
+import styled from "styled-components";
+import HamBtn from "./HamBtn";
+import { useNavigate } from "react-router-dom";
+import NavbarList from "./NavbarList";
+
+function Navbar() {
+
+  const navigate = useNavigate();
+
+  const moveToMain = () => {
+    navigate("/main") }
+
+  const [toggleOn, setToggleOn] = useState<boolean>(false);
+
+  const openMenu = () => {
+    setToggleOn(!toggleOn)
+  }
+  
+  return (
+    <div className="Navbar">
+      <StyledDiv>
+        <StyledImg onClick={moveToMain} className="logoImg" src="/img/logo2.png" alt="logo" />
+        <HamBtn onClick={openMenu}/>
+      </StyledDiv>
+      {toggleOn === true ?  <NavbarList/> : null} 
+
+    </div>
+  );
+}
+
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  position: sticky;
+  height: 60px;
+  width: 100%;
+  background-color: white;
+  margin: 0;
+  padding: 0;
+  border-bottom: 2px solid #D9D9D9;
+`;
+
+const StyledImg = styled.img`
+  margin-left: 8px;
+  margin-top: 1px;
+  height: 70px;
+`;
+
+export default Navbar;
---
+++ front/src/Pages/Start/Start.tsx
@@ -0,0 +1,9 @@
+function Start() {
+  return (
+    <div className="Start">
+      <h1>Start</h1>
+    </div>
+  );
+}
+
+export default Start;
