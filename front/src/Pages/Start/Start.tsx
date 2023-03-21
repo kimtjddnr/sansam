7c5962c1d5ff68a8b883e87306422ca661254aee
윤선영 <sunysa8@naver.com>
2023-03-21 13:33:35 +0900

    merge: ‘feat/191’ to ‘dev/fe’

--- front/src/Pages/Start/Start.tsx
+++ front/src/Pages/Start/Start.tsx
@@ -1,9 +1,25 @@
-function Start() {
+const Start = () => {
+  const KAKAO_AUTH_URL = "http://localhost:5000/oauth2/authorization/kakao";
+
+  const handleLogin = () => {
+    window.location.href = KAKAO_AUTH_URL;
+  };
+
   return (
     <div className="Start">
-      <h1>Start</h1>
+      <h1>Start입니다</h1>
+      <img
+        src="img/kakao_login_medium_narrow.png"
+        alt="login"
+        onClick={handleLogin}
+      />
+      <img
+        src="img/kakao_login_medium_wide.png"
+        alt="login_2"
+        onClick={handleLogin}
+      />
     </div>
   );
-}
+};
 
 export default Start;
