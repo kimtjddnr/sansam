2ac92994e6310e944ea703d4f58f0f86d2ca58b3
DonggiYang <luxdefuror@gmail.com>
2023-03-21 13:25:45 +0900

    bug: fixed saving tokens into sessionStorage.

--- front/src/Pages/Login/Login.tsx
+++ front/src/Pages/Login/Login.tsx
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function Login() {
   // navigation기능 사용을 위해 navigate 선언
   const navigate = useNavigate();
 
   // 주소값(localhost:3000/login?accessToken='코드'?refreshToken='코드') 받아오기 위해 searchParams 사용
-  // eslint-disable-next-line no-restricted-globals
-  const params = new URL(location.href).searchParams;
-  const accessToken: string | null = params.get("accessToken");
-  const refreshToken: string | null = params.get("refreshToken");
+  const location = useLocation();
+  const Token = location.search.split("?");
+  const accessToken = Token[1];
+  const refreshToken = Token[2];
 
   // 렌더링 되자마자 accessToken, refreshToken 세션 스토리지에 저장 후 메인페이지로 이동
   useEffect(() => {
