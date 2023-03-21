e8bad5c8ebb2677f2c5bea2fa90357947a99773b
yunseonyoung <sunysa8@naver.com>
2023-03-21 11:03:38 +0900

    feat: modify login function

--- front/src/Pages/Login/Login.tsx
+++ front/src/Pages/Login/Login.tsx
@@ -1,43 +1,22 @@
 import { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import axios from "axios";
-import { REST_API_KEY, REDIRECT_URI } from "../Start/KakaoLoginData";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-  const location = useLocation(); // 주소값(localhost:3000/login?code='인가코드') 받아오기 위해 useLocation 사용
-  const navigate = useNavigate(); // navigation기능 사용을 위해 navigate 선언
-  const KAKAO_CODE = location.search.split("=")[1]; // parameter로 전달받은 카카오측 인가코드 받아오기
+  // navigation기능 사용을 위해 navigate 선언
+  const navigate = useNavigate();
 
-  // 주소에서 인가코드 받아오는 또 다른 방법
-  // const params = new URL(document.location).searchParams;
-  // const KAKAO_CODE = params.get("code");
+  // 주소값(localhost:3000/login?accessToken='코드'?refreshToken='코드') 받아오기 위해 searchParams 사용
+  // eslint-disable-next-line no-restricted-globals
+  const params = new URL(location.href).searchParams;
+  const accessToken: string | null = params.get("accessToken");
+  const refreshToken: string | null = params.get("refreshToken");
 
-  // 카카오 토큰 받아오는 함수
-  const getKakaoToken = async (KAKAO_CODE: string) => {
-    axios
-      .post(
-        "https://kauth.kakao.com/oauth/token",
-        `grant_type=authorization_code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${KAKAO_CODE}`,
-        {
-          headers: {
-            "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-          },
-        }
-      )
-      .then((res) => {
-        if (res.data.access_token) {
-          localStorage.setItem("token", res.data.access_token); // 카카오 Access token 로컬 스토리지에 저장
-          console.log(res.data.access_token); // 카카오 Access token 콘솔에 출력
-        } else {
-          navigate("/");
-        }
-      });
-  };
-
-  // 인가 코드가 존재할 경우(초기 로그인) => 토큰 발급 함수 실행
+  // 렌더링 되자마자 accessToken, refreshToken 세션 스토리지에 저장 후 메인페이지로 이동
   useEffect(() => {
-    if (KAKAO_CODE) {
-      getKakaoToken(KAKAO_CODE);
+    if (accessToken && refreshToken) {
+      sessionStorage.setItem("accessToken", accessToken);
+      sessionStorage.setItem("refreshToken", refreshToken);
+      navigate("/main");
     }
   }, []);
 
--- front/src/Pages/Start/Start.tsx
+++ front/src/Pages/Start/Start.tsx
@@ -1,7 +1,5 @@
-import { REST_API_KEY, REDIRECT_URI } from "./KakaoLoginData";
-
 const Start = () => {
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  const KAKAO_AUTH_URL = "http://localhost:5000/oauth2/authorization/kakao";
 
   const handleLogin = () => {
     window.location.href = KAKAO_AUTH_URL;
@@ -23,17 +21,5 @@ const Start = () => {
     </div>
   );
 };
-
-// function Start() {
-//   return (
-//     <div className="Start">
-//       <h1>Start</h1>
-//       {/* <img src="img/kakao_login_large_narrow.png" alt="logo1" />
-//       <img src="img/kakao_login_large_wide.png" alt="logo1" /> */}
-//       <img src="img/kakao_login_medium_narrow.png" alt="logo2" />
-//       <img src="img/kakao_login_medium_wide.png" alt="logo2" />
-//     </div>
-//   );
-// }
 
 export default Start;
