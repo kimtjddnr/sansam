f5b08aa59b55a28aa480e7a73fe8d1ce72e70ad1
yunseonyoung <sunysa8@naver.com>
2023-03-22 17:55:49 +0900

    feat: detaching Axios Requests, modify mainSlice, save to axios response store, showing the list of recommendations

--- front/src/Pages/Main/List.tsx
+++ front/src/Pages/Main/List.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import ListItem from "./ListItem";
+import { useAppSelector } from "../../store/hooks";
+import { ItemInfo } from "../../store/mainSlice";
 
 const StyledDiv = styled.div`
   padding-left: 20px;
@@ -13,10 +15,31 @@ const StyledH = styled.p`
 `;
 
 function List() {
+  // store에 저장된 각 코스정보를 useSelector로 받아온다!
+  const genderAge: Array<ItemInfo> = useAppSelector(
+    state => state.main.genderAge
+  );
+  const easyCourse: Array<ItemInfo> = useAppSelector(
+    state => state.main.easyCourse
+  );
+  const normalCourse: Array<ItemInfo> = useAppSelector(
+    state => state.main.normalCourse
+  );
+  const hardCourse: Array<ItemInfo> = useAppSelector(
+    state => state.main.hardCourse
+  );
+
   return (
     <StyledDiv className="List">
       <StyledH>김머끄님을 위한 추천코스</StyledH>
-      <ListItem />
+      <ListItem
+        userAge={genderAge[0].userAge}
+        userGender={genderAge[0].userGender}
+        courses={genderAge[0].courses}
+      />
+      <ListItem courses={easyCourse[0].courses} />
+      <ListItem courses={normalCourse[0].courses} />
+      <ListItem courses={hardCourse[0].courses} />
     </StyledDiv>
   );
 }
