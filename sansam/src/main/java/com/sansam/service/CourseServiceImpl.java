0f9f1aa367dd572a1e5dfa4b1d0fc84e78e82e49
DonggiYang <xronace@gmail.com>
2023-03-23 14:47:10 +0900

    feat: implemented courseByNo API, refactored mountainList API to check accessToken at the same time.

--- sansam/src/main/java/com/sansam/service/CourseServiceImpl.java
+++ sansam/src/main/java/com/sansam/service/CourseServiceImpl.java
@@ -1,11 +1,17 @@
 package com.sansam.service;
 
+import com.sansam.data.entity.Coordinate;
+import com.sansam.data.entity.Course;
 import com.sansam.data.entity.Mountain;
+import com.sansam.data.repository.CoordinateRepository;
+import com.sansam.data.repository.CourseRepository;
 import com.sansam.data.repository.MountainRepository;
+import com.sansam.dto.response.CourseResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +20,8 @@ import java.util.List;
 public class CourseServiceImpl implements CourseService {
 
     private final MountainRepository mountainRepository;
+    private final CourseRepository courseRepository;
+    private final CoordinateRepository coordinateRepository;
 
     @Override
     public List<String> createMountainList() {
@@ -24,5 +32,19 @@ public class CourseServiceImpl implements CourseService {
         }
 
         return mountainList;
+    }
+
+    @Override
+    public CourseResponse getCourseDetails(int no) {
+        Course course = courseRepository.findCourseByCourseNo(no);
+        List<BigDecimal> courseXCoords = new ArrayList<>();
+        List<BigDecimal> courseYCoords = new ArrayList<>();
+        List<Coordinate> coordinates = coordinateRepository.findAllByCourseNo(no);
+        for (Coordinate coordinate : coordinates) {
+            courseXCoords.add(coordinate.getCoordX());
+            courseYCoords.add(coordinate.getCoordY());
+        }
+
+        return new CourseResponse(course.getCourseNo(), course.getCourseMtNm(), course.getCourseMtCd(), course.getCourseMtNo(), courseXCoords, courseYCoords, course.getCourseAbsDiff(), course.getCourseUptime(), course.getCourseDowntime(), course.getCourseLength(), course.getCourseLocation(), course.getCourseAddress());
     }
 }
