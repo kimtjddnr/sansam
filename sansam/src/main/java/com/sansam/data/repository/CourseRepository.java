87fd29302659d516c809b4a3fca8b05c71e1b49c
DonggiYang <xronace@gmail.com>
2023-03-23 14:45:42 +0900

    feat: added Course and Coordinate entities along with their repositories.

---
+++ sansam/src/main/java/com/sansam/data/repository/CourseRepository.java
@@ -0,0 +1,10 @@
+package com.sansam.data.repository;
+
+import com.sansam.data.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Integer> {
+    Course findCourseByCourseNo(int courseNo);
+}
