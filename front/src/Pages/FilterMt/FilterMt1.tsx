7627dc8052fa7c7420f2e99355fa89f74cce7c00
unknown <ahyun3089@naver.com>
2023-03-23 13:58:01 +0900

    feat: [FilterMt] / 유저가 선택한 버튼에 대한 css 구현 (onTime, onLength warning 수정은 필요)

--- front/src/Pages/FilterMt/FilterMt1.tsx
+++ front/src/Pages/FilterMt/FilterMt1.tsx
@@ -3,12 +3,18 @@ import styled from "styled-components"
 import Navbar from "../../Common/Navbar/Navbar";
 import ResultList from "../../Common/Result/ResultList";
 
-function FilterMt() {
+interface btnProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  onTime?: number;
+  onLength? :number;
+}
 
+function FilterMt(obj: btnProps) {
+  // console.log(obj.onTime)
   const time :string[] = ["전체", "1미만", "1-2", "2초과"];
-  const [onTime, setOnTime] = useState<string>("");
-
+  const [onTime, setOnTime] = useState<number>(0);
+  
   const length :string[] = ["전체", "1미만", "1-3", "3-5", "5초과"];
+  const [onLength, setOnLength] = useState<number>(0);
 
   const [searchMt, setSearchMt] = useState<object>({
     mt_nm : "",
@@ -25,6 +31,7 @@ function FilterMt() {
 
   console.log(searchMt);
   console.log(onTime);
+  console.log(onLength);
 
 
   return (
@@ -36,10 +43,11 @@ function FilterMt() {
       <StyledDiff>
         {time.map((data, index)=>{
           return (
-
             <StyledBtn 
               key={index} 
-              onClick={()=>{handleMt(data, "course_time");setOnTime(data);}}
+              onClick={()=>{handleMt(data, "course_time");setOnTime(index);}}
+              onTime={onTime}
+              index={index}
             >
               {data}
             </StyledBtn>
@@ -53,7 +61,14 @@ function FilterMt() {
       <StyledDiff>
         {length.map((data, index)=>{
           return (
-            <StyledBtn1 key={index} onClick={()=>handleMt(data, "course_length")}>{data}</StyledBtn1>
+            <StyledBtn1 
+              key={index} 
+              onClick={()=>{handleMt(data, "course_length");setOnLength(index);}}
+              onLength={onLength}
+              index={index}
+            >
+              {data}
+            </StyledBtn1>
           )
         })}
 
@@ -83,34 +98,31 @@ const StyledHr = styled.hr`
   border: 1px solid #E3E3E3;
 `
 
-const StyledBtn = styled.button`
+const StyledBtn = styled.button<{onTime: number; index: number}>`
   width: 20vw;
   height: 30px;
   background-color: white;
   box-shadow: 0 2px 4px rgba(0,0,0,0.25), 0 3px 3px rgba(0,0,0,0.22);
-  color: #818181;
+  color: ${(props) =>
+    props.index === props.onTime ? "#238C47" : "#818181"};
+  border: 2px solid ${(props) =>
+    props.index === props.onTime ? "#238C47" : "#818181"};
   font-size: 15px;
   font-family: "GmarketSansMedium";
-  border: 2px solid #818181;
-  ${({ key }) =>
-    key ? 'white' : '#737373'};
-  /* &:focus {
-    border: 2px #238C47 solid;
-    color: #238C47;
-  } */
   border-radius: 13px;
   margin: 5px;
-`
-
-const StyledBtn1 = styled.button`
+`;
+const StyledBtn1 = styled.button<{onLength: number; index: number}>`
   width: 16.5vw;
   height: 30px;
   background-color: white;
   box-shadow: 0 2px 4px rgba(0,0,0,0.25), 0 3px 3px rgba(0,0,0,0.22);
-  color: #818181;
+  color: ${(props) =>
+    props.index === props.onLength ? "#238C47" : "#818181"};
+  border: 2px solid ${(props) =>
+    props.index === props.onLength ? "#238C47" : "#818181"};
   font-size: 15px;
   font-family: "GmarketSansMedium";
-  border: 2px solid #818181;
   border-radius: 13px;
   margin: 3px;
 `
