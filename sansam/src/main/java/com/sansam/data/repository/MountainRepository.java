8e6fddb0ea6b81c5cd92ef65e671ce153f12f1d1
DonggiYang <xronace@gmail.com>
2023-03-23 14:44:13 +0900

    refactor: added repository annotation

--- sansam/src/main/java/com/sansam/data/repository/MountainRepository.java
+++ sansam/src/main/java/com/sansam/data/repository/MountainRepository.java
@@ -2,7 +2,9 @@ package com.sansam.data.repository;
 
 import com.sansam.data.entity.Mountain;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MountainRepository extends JpaRepository<Mountain, Integer> {
 
 }
