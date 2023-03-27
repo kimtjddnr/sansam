153b61597a92ba063f02cf68b5489b73bdf2a411
김유홍 <loyyir1223@naver.com>
2023-03-27 13:54:30 +0900

    Feat/244

--- front/src/Pages/SignUp2/Components/SearchBar.tsx
+++ front/src/Pages/SignUp2/Components/SearchBar.tsx
@@ -1,56 +1,130 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "../../../store/baseURL";
 
-interface SearchDatas {
-  mountain: string;
-}
+import stringSimilarity from "string-similarity";
+// import { Komoran } from 'koalanlp/API';
+
+// interface SearchDatas {
+//   mountain: string;
+// }
+
+// interface MtList {
+//   mountain: string;
+// }
+
 
 function SearchBar() {
+  // mtlist(axios로 받아오는 산 이름 값) useState 세팅
+  type MtList = [string] | []
+  const [mtlist, setMtlist] = useState<MtList>([""]);
+
+  // SearchBar가 랜더링되면 산목록 axios 받아서 mtlist에 저장
+  function getMtList() {
+    const AccessToken = sessionStorage.getItem("accessToken");
+    const RefreshToken = sessionStorage.getItem("refreshToken");
+    console.log(AccessToken, RefreshToken);
+
+    axios
+      .get("/course/mtlist", {
+        headers: {
+          "X-ACCESS-TOKEN": AccessToken,
+          "X-REFRESH-TOKEN": RefreshToken,
+        },
+      })
+
+      .then((res) => {
+        console.log("산목록 받기 성공");
+        console.log(res.data.mountainList);
+        setMtlist(res.data.mountainList);
+        console.log(mtlist);
+      })
+      .catch((err) => console.log("errror"));
+  }
+  useEffect(() => {
+    getMtList();
+  }, []);
 
   // keyword(검색창에 입력하는 값) useState 세팅
   const [keyword, setKeyword] = useState<string>("");
 
-
-  const [keyItems, setKeyItems] = useState<SearchDatas[]>([])
-
-  // 검색창에 입력하는 값을 keyword useState에 저장
-  const onChangeData = (e:React.FormEvent<HTMLInputElement>) => {
+  // 검색창 입력값 변할 때마다 입력하는 값을 keyword useState에 저장
+  const onChangeData = (e: React.FormEvent<HTMLInputElement>) => {
     setKeyword(e.currentTarget.value.trim());
-  console.log(keyword)
+    console.log(keyword);
+  };
+
+  // keyItem(알고리즘 돌린 결과값) useState 세팅
+  type SearchDatas = [string] | []
+  const initialSearchData : SearchDatas = [""]
+  const [keyItems, setKeyItems] = useState<SearchDatas>(initialSearchData);
+
+  // 자동완성을 위한 유사도 검사 알고리즘
+  function AutoSearchAlgo() {
+    type MatchData = [string]
+    const matches : MatchData = [""]
+    mtlist.forEach((mt) => {
+      const similarity = stringSimilarity.compareTwoStrings(mt, keyword);
+      if (similarity > 0.5) { // adjust this threshold value as needed
+        matches.push(mt)
+      }
+    })
+    setKeyItems(matches);
+    console.log(keyItems)
+  }
+  // 검색창에 keyword가 입력될 때 유사도 검사 알고리즘이 작동, 자연스러운 작동위해 딜레이를 줌
+  useEffect(() => {
+    const debounce = setTimeout(() => {
+      if (keyword) AutoSearchAlgo();
+    }, 100);
+    return () => {
+      clearTimeout(debounce);
+    };
+  }, [keyword]);
+
+
+  // 자동완성에 뜬 산이름을 클릭할 경우 발생하는 클릭이벤트
+  function AutoSearchClick() {
+    // 이거 메인페이지에 맞게 만들면 돼 선영아
+    // usestate로 값 받아서 검색창에 넣어주면 될듯?
   }
 
-  return(
+  return (
     <div>
       <SearchBarDiv>
-        <Search placeholder="산이름을 입력해주세요" value={keyword} onChange={onChangeData}/>
+        <Search
+          placeholder="산이름을 입력해주세요"
+          value={keyword}
+          onChange={onChangeData}
+        />
         {/* <SearchBtn>검 색</SearchBtn> */}
+
+        {/* 이거 돋보기 버튼이야 선영아 */}
         {/* <img src="/dotbogi.png" alt="dotbogi" /> */}
+        {/* styled div 안에 img로 css 수정할 수 있음 */}
       </SearchBarDiv>
-      {/* 임시조치된 상태입니다 */}
-      {
-        keyword ?
+
+      {/* 검색 키워드가 있어야 자동완성창을 보여줌 */}
+      {keyword ? (
         <AutoSearchDiv>
           <AutoSearchUl>
-            <AutoSearchLi>한라산</AutoSearchLi>
-            <AutoSearchLi>지리산</AutoSearchLi>
+            {(keyItems.slice(1,6)).map((mtname) => (
+              <AutoSearchLi key={mtname} onClick={AutoSearchClick}>{mtname}</AutoSearchLi>
+            ))}
           </AutoSearchUl>
-        </AutoSearchDiv> :
-        null
-      }
-
+        </AutoSearchDiv>
+      ) : null}
     </div>
-  )
+  );
   // return(
-    //   <div className='SearchBarDiv'>
-    //       <input className='SearchBar' type="text" name="" id="" placeholder=' 산 이름을 입력해주세요' /><button className='SearchBarBtn'>입 력</button>
-    //   </div>
-    // )
-  }
-  export default SearchBar
-
-
+  //   <div className='SearchBarDiv'>
+  //       <input className='SearchBar' type="text" name="" id="" placeholder=' 산 이름을 입력해주세요' /><button className='SearchBarBtn'>입 력</button>
+  //   </div>
+  // )
+}
+export default SearchBar;
 
 const SearchBarDiv = styled.div`
   width: 100vw;
@@ -60,66 +134,66 @@ const SearchBarDiv = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  img{
-    width : 8vw;
-    height : 8vw;
+  img {
+    width: 8vw;
+    height: 8vw;
     position: relative;
     right: 11vw;
   }
-`
+`;
 
 const Search = styled.input`
   width: 80vw;
   height: 18vw;
   border-radius: 20px;
   font-family: "GmarketSansMedium";
-  text-align : center;
+  text-align: center;
   font-size: 7.5vw;
   // margin-left : 7vw;
-  &: hover{
-    cursor : pointer;
+  &: hover {
+    cursor: pointer;
   }
-`
+`;
 
 const ClickedSearch = styled.input`
-width: 80vw;
-height: 18vw;
-border-radius: 20px;
-font-family: "GmarketSansMedium";
-font-size: 7vw;
-margin-left : 7vw;
-border : solid #238C47 1.5vw
-`
+  width: 80vw;
+  height: 18vw;
+  border-radius: 20px;
+  font-family: "GmarketSansMedium";
+  font-size: 7vw;
+  margin-left: 7vw;
+  border: solid #238c47 1.5vw;
+`;
 
 const AutoSearchDiv = styled.div`
   width: 80vw;
   height: 80vw;
-  background-color : #FFFFFF;
-  border : solid black 0.4vw;
-  position : absolute;
-  margin-left : 10vw;
-  border-radius : 10px;
-`
+  background-color: #ffffff;
+  border: solid black 0.4vw;
+  position: absolute;
+  margin-left: 10vw;
+  border-radius: 10px;
+`;
 
 const AutoSearchUl = styled.ul`
-  padding-left : 0vw;
-`
+  padding-left: 0vw;
+  padding-top : 2vw;
+`;
 
 const AutoSearchLi = styled.li`
   list-style-type: none;
-  font-size : 6vw;
-  font-weight : bold;
-  // margin-bottom : 3vw;
-  height : 10vw;
-  padding-top : 3vw;
+  font-size: 6vw;
+  font-weight: bold;
+  padding-bottom : 3vw;
+  height: 13vw;
+  padding-top: 3vw;
   border-radius: 10px;
   &: hover {
-    background-color : #EBEBEB;
-    cursor : pointer;
+    background-color: #ebebeb;
+    cursor: pointer;
   }
-  &: 
-`
-
+  &: ;
+`;
 
 // const SearchBtn = styled.button`
 //   width: 20vw;
