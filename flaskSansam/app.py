084351baa8813480d5831236f3a64a3657efcacf
김성욱 <kimtjddnr254@naver.com>
2023-03-27 22:09:07 +0900

    merge : 'feat/459' into 'dev/be'

--- flaskSansam/app.py
+++ flaskSansam/app.py
@@ -152,5 +152,44 @@ def get_course_by_area():
     return response
 
 
+@app.route("/course/search/mt", methods=["POST"])
+def get_course_by_mt_name():
+    access_token = request.headers.get("X-ACCESS-TOKEN")
+    refresh_token = request.headers.get("X-REFRESH-TOKEN")
+
+    email_response = get_email_response(access_token, refresh_token)
+    access_token = email_response.headers.get("X-ACCESS-TOKEN")
+
+    course_time = {0: (0, 50 * 60), 1: (0, 1 * 60), 2: (1 * 60, 2 * 60), 3: (2 * 60, 50 * 60)}
+    course_length = {0: (0, 50), 1: (0, 1), 2: (1, 3), 3: (3, 5), 4: (5, 50)}
+
+    res_dict = request.get_json()
+    course_mt_name = res_dict["courseMtNm"]
+    bt_len_no = res_dict["courseLengthBtNo"]
+    bt_time_no = res_dict["courseTimeBtNo"]
+
+    left_time, right_time = course_time[int(request.get_json()["courseTimeBtNo"])]
+    left_len, right_len = course_length[int(request.get_json()["courseTimeBtNo"])]
+
+    with database.connect() as conn:
+        courses = conn.execute(text(f"""
+        SELECT * 
+        FROM `COURSE`
+        WHERE `COURSE_MT_NM` = '{course_mt_name}'
+        AND `COURSE_UPTIME` + `COURSE_DOWNTIME` BETWEEN {left_time} AND {right_time} 
+        AND `COURSE_LENGTH` BETWEEN {left_len} AND {right_len}
+        """)).mappings().all()
+
+    courses = list(map(dict, courses))
+
+    body = {}
+    body["course_list"]=courses
+
+    response = make_response(body)
+    response.headers["X-ACCESS-TOKEN"] = access_token
+
+    return response
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
