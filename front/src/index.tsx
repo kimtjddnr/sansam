0d95e3c3bdcffa6b6121ed155773015f35aa9221
이진우 <blu_mountain@naver.com>
2023-03-29 17:23:26 +0900

    chore: [merge conflict 해결] / dev/fe와 합치며 발생한 conflict 해결

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -3,9 +3,6 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-
-serviceWorkerRegistration.register();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
