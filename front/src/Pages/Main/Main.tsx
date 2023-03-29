ed8c82226761102773a12e5404a430060dad8612
yunseonyoung <sunysa8@naver.com>
2023-03-29 09:03:30 +0900

    feat: 메인페이지 CORS 에러 수정

--- front/src/Pages/Main/Main.tsx
+++ front/src/Pages/Main/Main.tsx
@@ -60,11 +60,25 @@ function Main() {
       dispatch(changeAgeGender(res.data));
       // console.log(res.data);
     };
+    // axios 모듈화 안되어있는 코드
+    // const test = async () => {
+    //   const res = await axios.get(
+    //     "http://localhost:5001/course/main/age-gender",
+    //     {
+    //       headers: {
+    //         "X-ACCESS-TOKEN": accessToken,
+    //         "X-REFRESH-TOKEN": refreshToken,
+    //       },
+    //     }
+    //   );
+    //   console.log(res);
+    // };
     getGenderAge();
     getEasyCourse();
     getNormalCourse();
     getHardCourse();
     getageGender();
+    // test();
   }, []);
 
   return (
