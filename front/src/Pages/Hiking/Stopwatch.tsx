df25b1260d2fd874891f74328ffc46df713b7ce3
이진우 <blu_mountain@naver.com>
2023-03-31 10:28:35 +0900

    feat : [스탑워치, 사진찍기 기능 추가] / Stopwatch.tsx, Photo.tsx 파일테스트

--- front/src/Pages/Hiking/Stopwatch.tsx
+++ front/src/Pages/Hiking/Stopwatch.tsx
@@ -10,7 +10,7 @@ const Stopwatch = () => {
       const startTime = Date.now() - elapsedTime;
       intervalRef.current = window.setInterval(() => {
         setElapsedTime(Date.now() - startTime);
-      }, 10);
+      }, 1000);
     } else {
       window.clearInterval(intervalRef.current!);
     }
@@ -24,12 +24,12 @@ const Stopwatch = () => {
   };
 
   const formatElapsedTime = (time: number) => {
-    const minutes = Math.floor(time / 60000);
+    const hours = Math.floor(time / 3600000);
+    const minutes = Math.floor((time % 3600000) / 60000);
     const seconds = Math.floor((time % 60000) / 1000);
-    const milliseconds = Math.floor((time % 1000) / 10);
-    return `${minutes.toString().padStart(2, "0")}:${seconds
+    return `${hours.toString().padStart(2, "0")}:${minutes
       .toString()
-      .padStart(2, "0")}.${milliseconds.toString().padStart(2, "0")}`;
+      .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
   };
 
   return (
--- front/src/Pages/Photo/Photo.tsx
+++ front/src/Pages/Photo/Photo.tsx
@@ -1,12 +1,67 @@
-import Navbar from "../../Common/Navbar/Navbar";
+import React, { useRef, useState } from "react";
 
-function Photo() {
+function CameraApp() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [cameraStream, setCameraStream] = useState<MediaStream | null>(null);
+  const [imageDataUrl, setImageDataUrl] = useState<string>("");
+
+  const startCamera = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      setCameraStream(stream);
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+      }
+    } catch (error) {
+      console.error("Error accessing camera:", error);
+    }
+  };
+
+  const stopCamera = () => {
+    if (cameraStream) {
+      cameraStream.getTracks().forEach((track) => track.stop());
+      setCameraStream(null);
+    }
+  };
+
+  const captureImage = () => {
+    if (videoRef.current && canvasRef.current) {
+      const canvas = canvasRef.current;
+      canvas.width = videoRef.current.videoWidth;
+      canvas.height = videoRef.current.videoHeight;
+      const context = canvas.getContext("2d");
+      if (context) {
+        context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+        const dataUrl = canvas.toDataURL();
+        setImageDataUrl(dataUrl);
+      }
+    }
+  };
+
+  const downloadImage = () => {
+    const downloadLink = document.createElement("a");
+    downloadLink.href = imageDataUrl;
+    downloadLink.download = "image.png";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+
   return (
-    <div className="Photo">
-      <Navbar />
-      <h1>Photo</h1>
+    <div>
+      <h1>Camera App</h1>
+      <video ref={videoRef} autoPlay playsInline></video>
+      <canvas ref={canvasRef} style={{ display: "none" }}></canvas>
+      {cameraStream ? (
+        <button onClick={stopCamera}>Stop Camera</button>
+      ) : (
+        <button onClick={startCamera}>Start Camera</button>
+      )}
+      <button onClick={captureImage}>Capture Image</button>
+      {imageDataUrl && <button onClick={downloadImage}>Download Image</button>}
     </div>
   );
 }
 
-export default Photo;
+export default CameraApp;
