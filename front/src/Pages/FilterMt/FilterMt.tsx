b418dd28493329aea68459176af1c70aca53b1d8
unknown <ahyun3089@naver.com>
2023-04-03 23:43:25 +0900

    bug: [searchBar] / 메인페이지 서치바에서 필터페이지(산기반)으로 넘어왔을 때 다시 검색이 안되는 오류 해결

--- front/src/Pages/FilterMt/FilterMt.tsx
+++ front/src/Pages/FilterMt/FilterMt.tsx
@@ -43,7 +43,7 @@ function FilterMt() {
   // 검색창에 keyword가 입력될 때 키워드를 포함하고 있는 산들만 filter해주기
   useEffect(() => {
     setResultData(
-      mtList.filter(mountain => {
+      mtList.filter((mountain) => {
         if (mountain.includes(keyword) && keyword.length !== 0) {
           return mountain.includes(keyword);
         }
@@ -110,18 +110,16 @@ function FilterMt() {
           },
         }
       )
-      .then(res => {
+      .then((res) => {
         setCourseList(res.data.course_list);
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   };
 
   // 메인에서 필터페이지로 산 이름 넘겨줄 때 =========================
   const location = useLocation();
-
-  // 1. 산 이름
-  const mtName = decodeURI(location.pathname.slice(10));
-  console.log("mtName", mtName);
+  const [mtName, setMtName] = useState<string | null>(location.state);
+  // console.log(location.state);
 
   // 2. mtName을 axios객체에 넣기
   useEffect(() => {
@@ -129,6 +127,8 @@ function FilterMt() {
       handleMt(mtName, "courseMtNm");
     }
   }, [mtName]);
+
+  // useEffect(() => {}, [keyword]);
 
   // console 확인창 ===============================================
   // console.log(searchMt);
@@ -145,7 +145,7 @@ function FilterMt() {
             <InputDiv>
               <Search
                 placeholder="산이름을 입력해주세요"
-                defaultValue={mtName}
+                defaultValue={mtName || ""}
                 onChange={onChangeData}
                 onFocus={() => {
                   setIsFocus(true);
@@ -164,15 +164,14 @@ function FilterMt() {
                     resultData.map((result, index) => (
                       <Resultli
                         key={index}
-                        onMouseDown={e => {
+                        onMouseDown={(e) => {
                           e.preventDefault();
                         }}
                         onClick={() => {
-                          // console.log(result);
                           setKeyword(result);
+                          setMtName("");
                           handleMt(result, "courseMtNm");
                           setIsFocus(false);
-                          navigate(`/filtermt/${result}`);
                         }}
                       >
                         {result}
@@ -190,7 +189,7 @@ function FilterMt() {
             <InputDiv>
               <Search
                 placeholder="산이름을 입력해주세요"
-                value={keyword}
+                value={keyword || ""}
                 onChange={onChangeData}
                 onFocus={() => {
                   setIsFocus(true);
@@ -209,7 +208,7 @@ function FilterMt() {
                     resultData.map((result, index) => (
                       <Resultli
                         key={index}
-                        onMouseDown={e => {
+                        onMouseDown={(e) => {
                           e.preventDefault();
                         }}
                         onClick={() => {
@@ -279,6 +278,7 @@ function FilterMt() {
           onClick={() => {
             searchData();
             setPressSearch(!pressSearch);
+            initializeData();
           }}
         >
           검색
@@ -349,9 +349,10 @@ const StyledBtn = styled.button<ButtonInfo>`
   height: 30px;
   background-color: white;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.25), 0 3px 3px rgba(0, 0, 0, 0.22);
-  color: ${props => (props.index === props.timeState ? "#238C47" : "#818181")};
+  color: ${(props) =>
+    props.index === props.timeState ? "#238C47" : "#818181"};
   border: 2px solid
-    ${props => (props.index === props.timeState ? "#238C47" : "#818181")};
+    ${(props) => (props.index === props.timeState ? "#238C47" : "#818181")};
   font-size: 15px;
   font-family: "GmarketSansMedium";
   border-radius: 13px;
@@ -363,10 +364,10 @@ const StyledBtn1 = styled.button<ButtonInfo>`
   height: 30px;
   background-color: white;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.25), 0 3px 3px rgba(0, 0, 0, 0.22);
-  color: ${props =>
+  color: ${(props) =>
     props.index === props.lengthState ? "#238C47" : "#818181"};
   border: 2px solid
-    ${props => (props.index === props.lengthState ? "#238C47" : "#818181")};
+    ${(props) => (props.index === props.lengthState ? "#238C47" : "#818181")};
   font-size: 15px;
   font-family: "GmarketSansMedium";
   border-radius: 13px;
@@ -432,7 +433,7 @@ const Search = styled.input<SearchProps>`
   width: 100%;
   height: 11vw;
   border: 1px solid gray;
-  border-radius: ${props =>
+  border-radius: ${(props) =>
     props.isFocus ? "10px 10px 0px 0px" : "10px 10px"};
   font-family: "GmarketSansLight";
   text-align: left;
