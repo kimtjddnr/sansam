08c78e3a435e07f0ebf50b981fcd95f913cb87ba
이진우 <blu_mountain@naver.com>
2023-04-03 15:19:49 +0900

    Revert "Merge branch 'feat/530' into 'master'"
    This reverts merge request !92

--- front/src/Pages/MyPage/MyPage.tsx
+++ front/src/Pages/MyPage/MyPage.tsx
@@ -1,43 +1,7 @@
-import { useEffect, useState } from "react";
-import styled from "styled-components";
-import { Route, Routes, Link } from "react-router-dom";
-import MyHeart from "./MyHeart";
-import { userApi } from "../../api";
-import MyReview from "./MyReview";
-
-const StyledH2 = styled.h2`
-  padding-top: 8vw;
-  padding-left: 5vw;
-  margin: 0px;
-  font-family: "GmarketSansMedium";
-  font-size: 6vw;
-  margin-bottom: 5px;
-`;
-
 function MyPage() {
-  const accessToken = sessionStorage.getItem("accessToken");
-  const refreshToken = sessionStorage.getItem("refreshToken");
-
-  // 유저이름 state에 담아서 사용해주기 -> 나중에 시간 남으면 store에 유저정보 저장해주면 좋을듯
-  const [userName, setUserName] = useState<string>("");
-
-  // 처음 마운트 됐을 때
-  useEffect(() => {
-    // 현재 로그인한 유저정보 받아오는 코드
-    const getUserInfo = async () => {
-      const res = await userApi.userInfo(accessToken, refreshToken);
-      setUserName(res.data.userNicknm);
-    };
-    getUserInfo();
-  }, []);
-
   return (
     <div className="MyPage">
-      <StyledH2>{userName}님의 등산기록</StyledH2>
-      <Routes>
-        <Route path="/myheart" element={<MyHeart />} />
-        <Route path="/myreview" element={<MyReview />} />
-      </Routes>
+      <h1>MyPage</h1>
     </div>
   );
 }
