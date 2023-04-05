ac52265ac1791e90354aa985f4317dcf6a5ece25
unknown <ahyun3089@naver.com>
2023-04-05 09:46:25 +0900

    feat: [navbar] / 가입이 안된 유저에게는 navbar가 안뜸

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -14,27 +14,35 @@ import Photo from "./Pages/Photo/Photo";
 import MyPage from "./Pages/MyPage/MyPage";
 import NotFound from "./Pages/Exception/NotFound";
 import Navbar from "./Common/Navbar/Navbar";
+import { useAppSelector } from "./store/hooks";
 
 function App() {
+  const userNicknm: string = useAppSelector(
+    (state) => state.login.userInfo.userNicknm
+  );
+  console.log(userNicknm);
+
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Start />} />
-        <Route path="/login/*" element={<Login />} />
-        <Route path="/signup/1" element={<SignUp1 />} />
-        <Route path="/signup/2" element={<SignUp2 />} />
-        <Route path="/main/*" element={<Main />} />
-        <Route path="/filtermt/*" element={<FilterMt />} />
-        <Route path="/filterrg/*" element={<FilterRg />} />
-        <Route path="/coursedetail/*" element={<CourseDetail />} />
-        <Route path="/hiking/*" element={<Hiking />} />
-        <Route path="/review/*" element={<Review />} />
-        <Route path="/photo/*" element={<Photo />} />
-        <Route path="/mypage/*" element={<MyPage />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="app">
+      <BrowserRouter>
+        {userNicknm ? <Navbar /> : null}
+        <Routes>
+          <Route path="/" element={<Start />} />
+          <Route path="/login/*" element={<Login />} />
+          <Route path="/signup/1" element={<SignUp1 />} />
+          <Route path="/signup/2" element={<SignUp2 />} />
+          <Route path="/main/*" element={<Main />} />
+          <Route path="/filtermt/*" element={<FilterMt />} />
+          <Route path="/filterrg/*" element={<FilterRg />} />
+          <Route path="/coursedetail/*" element={<CourseDetail />} />
+          <Route path="/hiking/*" element={<Hiking />} />
+          <Route path="/review/*" element={<Review />} />
+          <Route path="/photo/*" element={<Photo />} />
+          <Route path="/mypage/*" element={<MyPage />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
