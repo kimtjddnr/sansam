83abd6881bea31be181450c8255d935e8aaa0e10
DonggiYang <xronace@gmail.com>
2023-04-07 01:55:42 +0900

    bug: added seperation of server in Swagger to enable Swagger to work directly at EC2 server.
    Former-commit-id: 0bffb5ff365c6e16faff220e84e0014b8ebae6df

---
+++ sansam/src/main/java/com/sansam/config/Workaround.java
@@ -0,0 +1,36 @@
+package com.sansam.config;
+
+import java.util.Arrays;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import springfox.documentation.oas.web.OpenApiTransformationContext;
+import springfox.documentation.oas.web.WebMvcOpenApiTransformationFilter;
+import springfox.documentation.spi.DocumentationType;
+
+@Component
+public class Workaround implements WebMvcOpenApiTransformationFilter {
+
+	@Override
+    public OpenAPI transform(OpenApiTransformationContext<HttpServletRequest> context) {
+        OpenAPI openApi = context.getSpecification();
+        Server serverForLocal = new Server();
+        serverForLocal.setDescription("local");
+        serverForLocal.setUrl("http://localhost:5000");
+
+        Server serverForEC2 = new Server();
+        serverForEC2.setDescription("EC2");
+        serverForEC2.setUrl("http://j8d205.p.ssafy.io:5000");
+        openApi.setServers(Arrays.asList(serverForLocal, serverForEC2));
+        return openApi;
+    }
+
+    @Override
+    public boolean supports(DocumentationType documentationType) {
+        return documentationType.equals(DocumentationType.OAS_30);
+    }
+}
