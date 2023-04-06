7753dffa29e60d990860eadb1dfae835c994e7e7
yunseonyoung <sunysa8@naver.com>
2023-04-06 10:02:05 +0900

    redux-persist 시도중

--- front/src/store/store.tsx
+++ front/src/store/store.tsx
@@ -5,6 +5,35 @@ import recommenReducer from "./RecommendSlice";
 import loginReducer from "./loginSlice";
 import courseReducer from "./courseSlice";
 
+// redux-persist
+import sessionStorage from "redux-persist/es/storage/session";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+// interface persistType {
+//   key: string;
+//   sessionStorage: any;
+// }
+
+const persistConfig: {
+  key: string;
+  sessionStorage: any;
+} = {
+  key: "root",
+  sessionStorage,
+  // whitelist: ['user']
+};
+
+const reducers = combineReducers({
+  main: mainReducer,
+  signup2: signup2Reducer,
+  recommend: recommenReducer,
+  login: loginReducer,
+  course: courseReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
   reducer: {
     main: mainReducer,
